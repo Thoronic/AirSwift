079f90b6b4075fa3839c7c40b0889c362c34fc0e
Thoronic <denib.qatlh@t-online.de>
2023-10-26 21:28:03 +0200

    Update data.sql

---
+++ src/Database/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO Customers (Name, PositionX, PositionY, PositionZ) VALUES
+('John Doe', 1.0, 2.0, 3.0),
+('Alice Smith', 2.5, 3.5, 1.0);
+
+INSERT INTO Orders (StartPosX, StartPosY, EndPosX, EndPosY, Weight, CustomerID, DroneID) VALUES
+-- Insertion statements for the Orders table
+
+INSERT INTO Drones (PositionX, PositionY, PositionZ, Status, BatteryStatus, OrderID) VALUES
+-- Insertion statements for the Drones table
