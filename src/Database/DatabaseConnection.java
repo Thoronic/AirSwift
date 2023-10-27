364417b6fe3fe199881aaaea54d578ee522579ed
Thoronic <denib.qatlh@t-online.de>
2023-10-27 08:50:19 +0200

    Update DatabaseConnection.java

--- src/Database/DatabaseConnection.java
+++ src/Database/DatabaseConnection.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
     public static Connection getConnection() {
         Connection connection = null;
