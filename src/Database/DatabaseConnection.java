51abe636de95f66e2eaa1cbc482aedc1b83cd1a7
Thoronic <denib.qatlh@t-online.de>
2023-10-27 08:48:53 +0200

    Update DatabaseConnection.java

--- src/Database/DatabaseConnection.java
+++ src/Database/DatabaseConnection.java
@@ -1,5 +1,23 @@
 package Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
-    
+    public static Connection getConnection() {
+        Connection connection = null;
+        String url = "jdbc:mysql://localhost:3306/MyDatabase"; // Update with your database URL
+        String user = "your_username"; // Replace with your MySQL username
+        String password = "your_password"; // Replace with your MySQL password
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
+
