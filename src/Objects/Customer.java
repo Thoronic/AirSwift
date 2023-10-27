7b2e32997d5cf2f594c10392f3d41779a86bd779
Thoronic <denib.qatlh@t-online.de>
2023-10-27 08:49:35 +0200

    Update Customer.java

--- src/Objects/Customer.java
+++ src/Objects/Customer.java
@@ -15,6 +15,34 @@ public class Customer{
         this.pos = pos;
     }
 
+    public static void insertCustomerData(String name, double posX, double posY, double posZ) {
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection == null) {
+            System.err.println("Failed to establish a database connection.");
+            return;
+        }
+
+        String insertSql = "INSERT INTO Customers (Name, PositionX, PositionY, PositionZ) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertSql)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setDouble(2, posX);
+            preparedStatement.setDouble(3, posY);
+            preparedStatement.setDouble(4, posZ);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public UUID getID(){
         return this.id;
     }
