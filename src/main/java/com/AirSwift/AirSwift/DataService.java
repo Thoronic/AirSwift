add01b97edebc2d84e498da5b2e151ede1a81aea
Thoronic <denib.qatlh@t-online.de>
2023-10-28 10:08:47 +0200

    Create DataService.java

---
+++ src/main/java/com/AirSwift/AirSwift/DataService.java
@@ -0,0 +1,8 @@
+@Service
+public class DataService {
+
+    public String processData(String requestData) {
+        // Perform data processing and return a result
+        return "Processed data: " + requestData;
+    }
+}
